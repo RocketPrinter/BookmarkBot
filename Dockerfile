003c6a61332de35baed909227aad86790f6dbae8
RocketPrinter <35203190+RocketPrinter@users.noreply.github.com>
2021-09-10 14:04:34 +0300

    overhauled docker building process for the 50th time IT'S WORKING NOW

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,11 @@
 ﻿# syntax=docker/dockerfile:1
 FROM mcr.microsoft.com/dotnet/sdk:5.0 as build
-
 WORKDIR /app
 COPY ./Server .
-
-#build
 RUN dotnet restore -v n -s https://api.nuget.org/v3/index.json -s https://nuget.emzi0767.com/api/v3/index.json
-RUN dotnet build -v n --no-restore 
+RUN dotnet publish -v n -o ./publish --no-restore 
 
-# install dotnet ef
-RUN dotnet tool install --global dotnet-ef
-ENV PATH $PATH:/root/.dotnet/tools
-
-# entrypoint script
-RUN chmod +x ./entrypoint.sh
-CMD /bin/bash ./entrypoint.sh 
+FROM mcr.microsoft.com/dotnet/aspnet:5.0
+WORKDIR /app
+COPY --from=build /app/publish .
+ENTRYPOINT ["dotnet", "Server.dll"]
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -11,19 +13,51 @@ namespace Server
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static bool inDocker => Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER") == "true";
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            // make sure db is up to date if we are running inside docker
+            if (inDocker)
+            {
+                using (var scope = host.Services.CreateScope())
+                {
+                    var logger = new Logger<Program>(host.Services.GetRequiredService<ILoggerFactory>());
+                    var db = scope.ServiceProvider.GetRequiredService<Db.BookmarkContext>();
+
+                    await Task.Delay(5000); // wait for db to start
+                    while (true)
+                    {
+                        try
+                        {
+                            logger.LogInformation("Migrating db...");
+                            await db.Database.MigrateAsync();
+                            break;
+                        }
+                        catch (Exception e)
+                        {
+                            logger.LogError(e, "Migration failed! Db probably hasn't started yet. Retrying after 5 seconds...");
+                            await Task.Delay(5000);
+                        }
+                    }
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureAppConfiguration((hostingContext,config) =>
                 {
+                    //if we are inside a container load the secret
                     // since the secret manager is only for development you can use this to specify the bot token and ConnectionStrings
-                    // intended to be used with a docker-compose secret
-                    // for some bizzare reason docker strips the file extension
-                    config.AddJsonFile("/run/secrets/server", true,false);
+                    if (inDocker)
+                    {
+                        // for some bizzare reason docker strips the file extension
+                        config.AddJsonFile("/run/secrets/server", false,false);
+                    }
                 })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
